601ed657663ac41f180d8ee68e9d92432ae35fc6
Pedro Neves <66841214+tyson-neves@users.noreply.github.com>
2021-01-21 20:44:44 -0300

    Create if_2.js

---
+++ if_2.js
@@ -0,0 +1,13 @@
+const nota = 8;
+const bomComportamento = true;
+
+if (nota >= 7 && bomComportamento)
+    console.log("Parabéns!!");
+
+
+
+if (nota < 7 || !bomComportamento) // ou negando o bomComportamento
+    console.log("É uma pena, você vai ter que se esforçar mais!!")    
+
+
+console.log("fim!");
