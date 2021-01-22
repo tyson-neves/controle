3f2d6cd45fbccdd14660633a1670245dabbf3184
Pedro Neves <66841214+tyson-neves@users.noreply.github.com>
2021-01-21 23:30:38 -0300

    Create if_3.js

---
+++ if_3.js
@@ -0,0 +1,9 @@
+const nota = 90;
+
+if (nota >= 90) {
+
+    console.log("Quadro de honra ");
+    console.log("Parabéns");
+} // quando vc quiser colocar algo no if que seja um bloco de código (mais de uma sentença você precisa usar as "{ }")
+
+console.log("Fim!");    
