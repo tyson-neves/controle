a7163067c94a8b2b68bb1f8b309d11add0d9afb0
Pedro Neves <66841214+tyson-neves@users.noreply.github.com>
2021-01-22 23:22:58 -0300

    Desafio IF

---
+++ desafio_if.js
@@ -0,0 +1,23 @@
+const nota = 6.5;
+
+if (nota >= 9 && nota <=10) {
+    console.log("Parabéns o seu conceito é A!!");
+}
+
+if (nota <= 8.9 && nota >=7) {
+    console.log("Parabéns o seu conceito é B!");
+}
+
+if (nota <= 6.9 && nota >=5) {
+    console.log("Sua conceito foi C, vamos estudar mais!");
+}
+if (nota <= 4.9 && nota >= 4.5){
+    console.log("Seu conceito foi D, você precisa de reforço!");
+}
+
+if (nota <=4.5){
+    console.log("Seu conceito é D e você precisa urgentemente de reforço especial para não ser reprovado!!");
+}
+
+
+console.log("Fim do programa");
