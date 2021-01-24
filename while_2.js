ab3b082bee537a323297f44acbf998b1e4803dea
Pedro Neves <66841214+tyson-neves@users.noreply.github.com>
2021-01-24 19:44:48 -0300

    Create while_2.js

---
+++ while_2.js
@@ -0,0 +1,7 @@
+let quantidade = 0;
+
+while (Math.random() < 0.9) { // enquanto ela for verdadeira ela vai repetir (indeterminadamente)
+    quantidade++; // somar +1
+}
+
+console.log("Qtde: " + quantidade);
