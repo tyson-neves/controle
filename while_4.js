3ce60af7c900daed89daa590738296512a9f99ac
Pedro Neves <66841214+tyson-neves@users.noreply.github.com>
2021-01-24 20:18:34 -0300

    while 3 e 4

---
+++ while_4.js
@@ -0,0 +1,10 @@
+let controle = 1; // DECLARAÇÃO
+
+while (controle <= 10) { // EXPRESSÃO
+    console.log(controle);
+    controle++; // INCREMENTO
+}
+
+console.log("Fim!");
+
+// colocando uma quantidade determinada em uma estrutura indefinida, isso pq tivemos a DECLARAÇÃO, EXPRESSÃO E INCRIMENTO que fazem ela ser uma bloco de código definido.
