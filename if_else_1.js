5ff2acee80a406931a157f9c0d950adbff25d15a
Pedro Neves <66841214+tyson-neves@users.noreply.github.com>
2021-01-23 23:51:07 -0300

    Create if_else_1.js

---
+++ if_else_1.js
@@ -0,0 +1,17 @@
+const verdadeiro = true;
+const falso = false;
+
+if (verdadeiro) {
+    console.log("Exec");
+}
+else {
+    console.log("Nào exec");    
+}
+if (falso) {
+    console.log("Exec");
+}
+else {
+    console.log("Não exec");
+}
+
+// else é o famoso senão ... ou seja... se isso faça isso... se não.. faça aquilo.
