bb3279dcd4d7640f2588c292b1398c0a087b9cb5
Pedro Neves <66841214+tyson-neves@users.noreply.github.com>
2021-01-24 12:01:17 -0300

    if_else aula 4

---
+++ if_else_4.js
@@ -0,0 +1,17 @@
+// esse é somente uma forma diferente de escrever a estrutura da aula passada usando else if juntos para definir as coisas
+
+const hora = 11;
+let saudacao;
+
+if (hora < 12) {
+    saudacao = "Bom Dia!";
+} else if (hora < 18) {    
+        saudacao = "Boa tarde!";
+} else if (hora < 22) {
+        saudacao = "Boa Noite!";
+} else {
+    saudacao = "Muito tarde, vá para a cama"
+}
+
+
+console.log(saudacao);
