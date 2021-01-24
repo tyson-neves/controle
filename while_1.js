815ad34d70a18bb51062beb5ee1bfe39e6cd4ff6
Pedro Neves <66841214+tyson-neves@users.noreply.github.com>
2021-01-24 19:20:43 -0300

    Create while_1.js

---
+++ while_1.js
@@ -0,0 +1,8 @@
+// PARA EXECUTAR O CODIGO CTRL ALT N
+// PARA PARAR A EXECUÇÃO DE UM CODIGO CTRL ALT M
+
+while (true) {
+    console.log("Loop infinito");
+} // enquanto o valor for verdadeiro ele vai executar... como ela é true ela vai executar 4ever
+
+console.log("Fim!");
