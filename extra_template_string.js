5a51844549e18c62deca24a7eadb18b2e63f6628
Pedro Neves <66841214+tyson-neves@users.noreply.github.com>
2021-01-25 15:07:46 -0300

    Create extra_template_string.js

---
+++ extra_template_string.js
@@ -0,0 +1,7 @@
+const i = 2;
+const j = 9;
+
+                       //template string       template string
+console.log(` O valor de i é ${i}, e o valor de j é ${j}`);
+console.log(`1 + 2 = ${1 + 2}`);
+console.log(`Valor aleatório gerado foi: ${Math.random()}`);
