7c4a710ed206678963571de90dcc4807f6eb01f8
Pedro Neves <66841214+tyson-neves@users.noreply.github.com>
2021-01-25 12:58:58 -0300

    Create for_2.js

---
+++ for_2.js
@@ -0,0 +1,14 @@
+// Criação de laço infinito usando For ( Não executar o código pois pode crashar a maquina)
+
+
+for (;;) {
+
+}
+
+console.log("Fim!");
+
+
+for (; true;) {
+}
+
+console.log("Fim!")
