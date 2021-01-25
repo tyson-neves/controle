37385ca60b703cfff5a4cfa858ded0b7538b246a
Pedro Neves <66841214+tyson-neves@users.noreply.github.com>
2021-01-25 15:50:50 -0300

    for 4 e desafio

---
+++ desafio_for.js
@@ -0,0 +1,14 @@
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// fazer isso sem números
+
+let linha = "";
+
+for(let i = "#"; i!= "#######"; i += "#") {
+    linha += "#";
+    console.log(linha);
+}
